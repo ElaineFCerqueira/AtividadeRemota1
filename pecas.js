b9738ff6e354b28f170f9374cdc10c74552cc97c
Elaine <elainecerqueira@conder.ba.gov.br>
2022-08-02 12:56:46 -0300

    alteração no quantitativo de peças

--- pecas.js
+++ pecas.js
@@ -1,8 +1,8 @@
-let quantidadePeca = 15 // A Quantidade de peças não pode ser maior que 10
+let quantidadePeca = 8 // A Quantidade de peças não pode ser maior que 10
 let pesoPeca = 200 // O peso deve ser maior que 100g
 let nomePeca = "Polia" // O tamanho do nome deve ser maior que 2 caracteres
 
-if (quantidadePeca < 10) {
+if (quantidadePeca < 7) {
     console.log ("Quantidade de peça dentro do esperado. O produto do ser cadastrado")
 
     if (pesoPeca >= 100) {
@@ -14,7 +14,7 @@ if (quantidadePeca < 10) {
         } else {
             console.log ("nome com menos de 2 caracteres, peça não cadastrada")
         }
-        
+
     } else {
         console.log ("Peso maior que 100, peça não cadastrada")
     }
