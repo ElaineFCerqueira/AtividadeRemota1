6eeb86eea25e9c041375c98159e5e94c653f2187
Elaine <elainecerqueira@conder.ba.gov.br>
2022-08-02 12:45:41 -0300

    alteração de lugar das variavéis

--- pecas.js
+++ pecas.js
@@ -1,12 +1,6 @@
-// O peso deve ser maior que 100g
-// O tamanho do nome deve ser maior que 2 caracteres
-// A Quantidade de peças não pode ser maior que 10
-
-
-
-let pesoPeca = 200
-let nomePeca = "Polia"
-let quantidadePeca = 15
+let quantidadePeca = 15 // A Quantidade de peças não pode ser maior que 10
+let pesoPeca = 200 // O peso deve ser maior que 100g
+let nomePeca = "Polia" // O tamanho do nome deve ser maior que 2 caracteres
 
 if (quantidadePeca < 10) {
     console.log ("Quantidade de peça dentro do esperado. O produto do ser cadastrado")
@@ -14,13 +8,13 @@ if (quantidadePeca < 10) {
     if (pesoPeca >= 100) {
         console.log ("Peso ok!, peça cadastrada com sucesso")
 
-        if (nomePeca > 2) {
+        if (nomePeca.length > 2) {
             console.log ("Nome ok!")
 
         } else {
             console.log ("nome com menos de 2 caracteres, peça não cadastrada")
         }
-
+        
     } else {
         console.log ("Peso maior que 100, peça não cadastrada")
     }
